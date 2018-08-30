c9e414d7d733cca604dd4cf0a85e61ccb60f0cad
xuehenzcc <1255029471@qq.com>
2018-08-30 19:03:18 +0800

    # WARNING: head commit changed in the meantime
    zcc,company

---
+++ src/main/java/com/zcc/game/utils/ScheduledTasks.java
@@ -0,0 +1,36 @@
+/**
+ * 版权：zcc
+ * 作者：c0z00k8
+ * @data 2018年8月30日
+ */
+package com.zcc.game.utils;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.zcc.game.service.HomeService;
+
+/**
+ * @author c0z00k8
+ *
+ */
+@Component
+@EnableScheduling
+public class ScheduledTasks {
+
+	@Autowired
+	private HomeService homeService;
+	
+	
+	@Scheduled(cron = "0 0/1 * * * ?")  //cron接受cron表达式，根据cron表达式确定定时规则
+    public void testCron() {
+        System.out.println("time:"+new Date());
+        homeService.taskData();
+    }
+
+	
+}
---
+++ src/main/java/com/zcc/game/vo/base/OpenData.java
@@ -0,0 +1,44 @@
+/**
+ * 版权：zcc
+ * 作者：c0z00k8
+ * @data 2018年8月30日
+ */
+package com.zcc.game.vo.base;
+
+/**
+ * @author c0z00k8
+ *
+ */
+public class OpenData {
+
+	private String expect;
+	private String opencode;
+	private String opentime;
+	private Long opentimestamp;
+	public String getExpect() {
+		return expect;
+	}
+	public void setExpect(String expect) {
+		this.expect = expect;
+	}
+	public String getOpencode() {
+		return opencode;
+	}
+	public void setOpencode(String opencode) {
+		this.opencode = opencode;
+	}
+	public String getOpentime() {
+		return opentime;
+	}
+	public void setOpentime(String opentime) {
+		this.opentime = opentime;
+	}
+	public Long getOpentimestamp() {
+		return opentimestamp;
+	}
+	public void setOpentimestamp(Long opentimestamp) {
+		this.opentimestamp = opentimestamp;
+	}
+	
+	
+}
