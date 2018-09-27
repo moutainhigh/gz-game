5d66599661990a77e5434bcfd944bbc56227413e
xuehenzcc <1255029471@qq.com>
2018-09-27 22:15:12 +0800

    张聪聪-180927

---
+++ src/main/java/com/zcc/game/vo/ParamVO.java
@@ -0,0 +1,66 @@
+package com.zcc.game.vo;
+
+import java.util.Date;
+
+public class ParamVO {
+	  
+	private String id;
+	private String number;
+	private String name;
+	private String data;
+	private String remark;
+	private String auto;
+	private Date createdate;
+	private Date lastupdate;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public String getAuto() {
+		return auto;
+	}
+	public void setAuto(String auto) {
+		this.auto = auto;
+	}
+	public Date getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(Date createdate) {
+		this.createdate = createdate;
+	}
+	public Date getLastupdate() {
+		return lastupdate;
+	}
+	public void setLastupdate(Date lastupdate) {
+		this.lastupdate = lastupdate;
+	}
+	
+	
+	
+}
