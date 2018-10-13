a585ee8e2c1e0cdc0a5932405ded7713103ed31f
xuehenzcc <1255029471@qq.com>
2018-10-13 12:34:23 +0800

    张聪聪--修改上传图片接口

--- src/main/java/com/zcc/game/controller/UserController.java
+++ src/main/java/com/zcc/game/controller/UserController.java
@@ -6,6 +6,8 @@
 package com.zcc.game.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -28,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.alibaba.fastjson.JSON;
 import com.zcc.game.common.CommonUtil;
@@ -401,7 +404,7 @@ public class UserController extends BaseController{
 	 * @throws IOException 
 	 */
 	@RequestMapping("updateImage")
-	public void updateImage(HttpServletRequest request, HttpServletResponse response) throws Exception{
+	public void updateImage(HttpServletRequest request, HttpServletResponse response,@RequestParam MultipartFile[] file) throws Exception{
 		String[] paramKey = {"userId","type"};
         Map<String, String> params = parseParams(request, "updateImage", paramKey);
         String userId = params.get("userId");
@@ -413,7 +416,8 @@ public class UserController extends BaseController{
         //1 头像
 //        try {
 			//上传图片并获取路径
-        	List<String> imageUrls = imageService.upload(request, UploadType.USER_PIC);
+//        	List<String> imageUrls = imageService.upload(request, UploadType.USER_PIC);
+        	List<String> imageUrls =fileUpload(file);
         	if(imageUrls==null || imageUrls.size()==0){
         		renderJson(request, response,  SysCode.SYS_ERR, null);
         		return;
@@ -444,7 +448,7 @@ public class UserController extends BaseController{
 	 */
 	@ResponseBody
 	@RequestMapping("/file/upload")
-	public Map<String, Object> fileUpload( @RequestParam MultipartFile[] file) throws IOException{
+	public List<String> fileUpload( @RequestParam MultipartFile[] file) throws IOException{
 		
 		List<String> urls = new ArrayList<String>();
 		Map<String, Object> result = new HashMap<String, Object>();
@@ -464,6 +468,9 @@ public class UserController extends BaseController{
 //			            /usr/local/nginx/html/pic
 //			            String realPath = request.getSession().getServletContext().getRealPath("/upload")+ File.separator +cdate; 
 			            String r="/usr/local/nginx/html/pic"+ File.separator +cdate;
+			            if(!new File(r).exists()){
+			    			new File(r).mkdir();
+			    		}
 			            String realPath = "D:\\projects\\bak\\springboot-adminlte-admin-master\\src\\main\\resources\\upload"+ File.separator +cdate; 
 			            FileUtils.copyInputStreamToFile(myfile.getInputStream(), new File(r, reName)); 
 			            urls.add("/pic/"+cdate+"/"+reName);
@@ -471,14 +478,67 @@ public class UserController extends BaseController{
 			    }
 			result.put("status", "success");
 			result.put("urls",urls);
-			return result;
+			return urls;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			result.put("status", "error");
-			return result;
+			return urls;
 		}  
 	}
 
+	@RequestMapping("/upload")
+	@ResponseBody
+	public String testUploadFile(HttpServletRequest req,MultipartHttpServletRequest multiReq) throws IOException{
+//		List<String> urls = new ArrayList<String>();
+//		Map<String, Object> result = new HashMap<String, Object>();
+//		
+//		try {
+//			for(MultipartFile myfile : file){  
+//			        if(myfile.isEmpty()){  
+//			        	logger.warn("文件未上传");  
+//			        }else{  
+//			            logger.debug("文件长度: " + myfile.getSize());  
+//			            logger.debug("文件类型: " + myfile.getContentType());  
+//			            logger.debug("文件名称: " + myfile.getName());  
+//			            logger.debug("文件原名: " + myfile.getOriginalFilename());  
+//			            String ext =  FilenameUtils.getExtension(myfile.getOriginalFilename());
+//			            String reName = RandomStringUtils.randomAlphanumeric(32).toLowerCase() + "."+ ext;
+//			            String cdate = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
+////			            String realPath = request.getSession().getServletContext().getRealPath("/upload")+ File.separator +cdate; 
+//			            String realPath = "D:\\projects\\bak\\springboot-adminlte-admin-master\\src\\main\\resources\\upload"+ File.separator +cdate; 
+//			            FileUtils.copyInputStreamToFile(myfile.getInputStream(), new File(realPath, reName)); 
+//			            urls.add("/upload/"+cdate+"/"+reName);
+//			        }  
+//			    }
+//			result.put("status", "success");
+//			result.put("urls",urls);
+//			return result;
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			result.put("status", "error");
+//			return result;
+//		}  
+//		String path="/home/upload/1.jpg";
+		String ext =  FilenameUtils.getExtension(multiReq.getFile("file").getOriginalFilename());
+        String reName = RandomStringUtils.randomAlphanumeric(32).toLowerCase() + "."+ ext;
+        String cdate = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
+		String path="/usr/local/nginx/html/pic/"+cdate;
+		if(!new File(path).exists()){
+			new File(path).mkdir();
+		}
+//		"F:\\upload\\1.jpg"
+		FileOutputStream fos=new FileOutputStream(new File(path+File.separator+reName));
+		FileInputStream in = (FileInputStream) multiReq.getFile("file").getInputStream();
+		byte [] b=new byte[1024];
+		int len=0;
+		while ((len=in.read(b))!=-1) {
+			fos.write(b, 0, len);
+		}
+		fos.close();
+		in.close();
+		return "pic/"+cdate+File.separator+reName;
+	}
 	
 }
